235b937bb6f91b98d5e979ed6d44c353b3aa6f0c
Paul <paul.du56@gmail.com>
2021-03-14 11:14:48 +0100

    Only logs portfolio's logs

--- roles/tools/files/getlogs.sh
+++ roles/tools/files/getlogs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# TODO: Move to syslog 
 mkdir -p /var/log/portfolio/
 while read id; do
         docker logs "$id" > "/var/log/portfolio/portfolio.$id.""$(date +%Y-%m-%d)".log
-done< <(docker ps -aq)
+done< <(docker ps -aq --filter ancestor=portfolio)
